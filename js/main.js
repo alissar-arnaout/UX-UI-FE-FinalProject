f633220ffe4932dc419a152aa88136ede21b683a
Lil' Sheffield <masheffield13@gmail.com>
2021-06-28 18:15:54 -0600

    commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  var morebttn = document.getElementById("morebttn");
+  var learnmore = document.getElementById("learnmore");
+
+  if() {
+    learnmore.innerHTML = "Learn More"; 
+    morebttn.style.display = "none";
+  };
+  else {
+    learnmore.innerHTML = "Learn Less"; 
+    morebttn.style.display = "inline";
+  };
+}
